f7c22f2b16c93b69b73710a9a7a6573660d64188
alex <you@noneofyourbusiness.com>
2024-07-10 00:00:19 -0500

    grr

--- NNPACK-darknet/build/FXdiv/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alex/NNPACK-darknet/deps/fxdiv
-# Build directory: /home/alex/NNPACK-darknet/build/FXdiv
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
